9223a78a9e2f31b735902a991ff418463b6c1715
Jeffers199817 <jefferson.alquinga1998@gmail.com>
2025-04-12 23:32:02 -0500

    iNTEGRACIÓN DE FUNSDAMENTOS DE C#'

---
+++ FundamentosCSHARP/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace FundamentosCSHARP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Byte normal que tiene 1 byte contiene 8 bit. 
+            byte numero = 255;
+
+            //Byte tiene su nemesis que utilisa la s para negativos y reduce a la mitad los valores -127 128
+            sbyte numero1 = -127;
+            //Int que te permite tener 
+            int numero2 = -23423;
+            //Int tine su nemesis que es u que permite solo valores positivos
+            uint numero3 = 333434534;
+
+            long numero4 = 3345453;
+            ulong numero5 = 34534;
+
+            float numero6 = 189.1f;
+
+            double numero = 189.3d;
+
+
+
+
+
+            
+
+            
+        }
+    }
+}
